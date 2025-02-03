e7c8c64a72848b7c2ffc4e3db38844bbb150058f
Faith <ardist456@gmail.com>
2025-02-02 23:20:19 -0500

    i did it
    Yep, that was hard.

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
 // 1. Import the all three objects (Song, Musician, Playlist) that you created in the MusicData.js module file
-
+import { Song, Musician, Playlist } from "./MusicData.js";
 
 // 2. declare a new constant and assign to it a new musician object to describe the musician for the first song.
-
-
+const musician1 = new Musician("Marco Luka", "Guitar", "Rock");
 
 // 3. declare a new constant and assign to it a new musician object to describe the musician for the second song.
-
-
-
+const musician2 = new Musician("Dominic Fike", "Guitar", "Rock");
 
 // 4. declare a new constant and assign to it a new musician object to describe the musician for the third song.
-
+const musician3 = new Musician("Frank Ocean", "Keyboard", "R&B");
 
 // 5. declare a new constant and assign to it a new Song object to describe the first song
 // (make sure to use the Musician object from step 2 in creating this Song object
-
-
+const song1 = new Song("rush", musician1, "need you now");
 
 // 6. declare a new constant and assign to it a new Song object to describe the second song
 // (make sure to use the Musician object from step 3 in creating this Song object
-
+const song2 = new Song("Superstar Sh*t", musician2, "What Could Possibly Go Wrong");
 
 // 7. declare a new constant and assign to it a new Song object to describe the third song
 // (make sure to use the Musician object from step 4 in creating this Song object
-
+const song3 = new Song("Pink + White", musician3, "Blonde");
 
 // 8. declare a new constant called myPlaylist and assign to it a new Playlist object and make sure to use the three Song objects
 //from steps 5, 6, & 7 when creating it.
 //It must be called myPlaylist
-//It must be called myPlaylist
-
+const myPlaylist = new Playlist("myPlaylist", song1, song2, song3);
 
 //9. call the .getInfo() method on myPlaylist
-
+console.log(myPlaylist.getInfo());
 
 //DO NOT EDIT BELOW THIS LINE
 //the code below here will insert the Playlist data into the webpage.
@@ -45,5 +40,4 @@ bodyHTML.innerHTML += `
         <li><em>${myPlaylist.song2.title}</em> by ${myPlaylist.song2.artist.name}</li>
         <li><em>${myPlaylist.song3.title}</em> by ${myPlaylist.song3.artist.name}</li>
     </ol>
-
 `;
